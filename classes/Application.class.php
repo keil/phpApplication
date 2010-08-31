d3585790121dcb031a98e0a3f8c4958075e9ccb0
Matthias Keil <keil@rm-keil.de>
2010-08-31 11:38:05 +0200

    Application.class.php
    Signed-off-by: Matthias Keil <keil@rm-keil.de>

--- classes/Application.class.php
+++ classes/Application.class.php
@@ -2,18 +2,24 @@
 
 /**************************************************
  * APPLICATION
- * php application fileloader
  **************************************************/
 
 /**************************************************
- * Author: Roman Matthias Keil
- * Copyright: Roman Matthias Keil
- * Publisher: rm-keil.de
+ * @package application
+ * @version 1.10 $Revision: 803 $
  **************************************************/
 
 /**************************************************
- * $Date: 2010-05-19 13:45:33 +0200 (Mi, 19 Mai 2010) $
- * $Revision: 800 $
+ * @author: Roman Matthias Keil
+ * @copyright: Roman Matthias Keil
+ **************************************************/
+
+/**************************************************
+ * $Id: Application.class.php 803 2010-05-20 13:47:08Z webadmin $
+ * $HeadURL: http://svn.rm-keil.de/rm-keil/webpages/matthias-keil.de/Release%20(1.0)/httpdocs/_app/Application.class.php $
+ * $Date: 2010-05-20 15:47:08 +0200 (Do, 20 Mai 2010) $
+ * $Author: webadmin $
+ * $Revision: 803 $
  **************************************************/
 
 Application::initialize($_SERVER["DOCUMENT_ROOT"].'/_app');
@@ -26,44 +32,41 @@ class Application {
 		Application::$include = Application::scan($_root);
 	}
 
-	private static function scan($_path) {
-		$delimiter = "/";
+	private static function scan($_path, $_prefix = '') {
+		$delimiter = '/';
+		$separator = '.';
+		$wildcard = '*';
 		$result = array();
+		$files = array();
 
 		$handle = opendir($_path);
 		while(false !== ($resource = readdir($handle))) {
-			if ($resource == "." || $resource == ".." || $resource == ".svn" || $resource == ".htaccess" || $resource == ".htpasswd") continue;
-
-			if(is_dir($_path.$delimiter.$resource)) {
-				$result[$resource] = Application::scan($_path.$delimiter.$resource);
-			}
-			else if(is_file($_path.$delimiter.$resource)) {
-				$result[substr($resource, 0, strpos($resource, '.'))] = $_path.$delimiter.$resource;
+			if ($resource == "." || $resource == ".." || $resource == ".svn" || $resource == ".htaccess" || $resource == ".htpasswd") {
+				continue;
+			} else if(is_dir($_path.$delimiter.$resource)) {
+				$result = array_merge($result, Application::scan($_path.$delimiter.$resource, $_prefix.$resource.$separator));
+				continue;
+			}else if(is_file($_path.$delimiter.$resource)) {
+				$files[] = $_path.$delimiter.$resource;
+				$result[$_prefix.substr($resource, 0, strpos($resource, '.'))] = $_path.$delimiter.$resource;
+				continue;
 			}
 		}
+
+		$result[$_prefix.$wildcard] = $files;
 		closedir($handle);
 		return $result;
 	}
 
 	static function import($_include) {
-
-		$packages = explode('.', $_include);
-		$file = array_pop($packages);
-
-		$includes = Application::$include;
-
-		foreach($packages as $package)
-			if(isset($includes[$package]))
-				$includes = $includes[$package];
-			else
-				throw new RuntimeException('undefinded package: '.$package);
-
-		if($file != '*') {
-			if(is_file($includes[$file])) require_once $includes[$file];
-		}
-		else {
-			foreach($includes as $include) {
-				if(is_file($include)) require_once $include;}
+		if(!array_key_exists($_include, Application::$include)) {
+			throw new RuntimeException('undefinded package: '.$package);
+		} else if(is_array(Application::$include[$_include])) {
+			foreach (Application::$include[$_include] as $file) {
+				require_once $file;
+			}
+		} else {
+			require_once Application::$include[$_include];
 		}
 	}
 }
